bb814dc29f037f726e67c47d35c2384e775da239
cengamat <azurnaci@cultwinesltd.com>
2022-10-17 16:36:06 +0100

    Pass down search result and render result list

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Track } from "../Track/Track";
 
 export default class TrackList extends Component {
   render() {
     return (
-        <div className="TrackList">
-        <!-- You will add a map method that renders a set of Track components  -->
-    </div>
-    )
+      <div className="TrackList">
+        {this.props.tracks.map((track) => {
+          <Track key={track.id} track={track} />;
+        })}
+      </div>
+    );
   }
 }
