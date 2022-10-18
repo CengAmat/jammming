7e7be6bae3833da69c2a98ecf824839689427d86
cengamat <azurnaci@cultwinesltd.com>
2022-10-18 15:21:05 +0100

    Implement Spotify Search Request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,8 @@
+import { SearchBar } from "../Components/SearchBar/SearchBar";
+
 const clientId = "e50e28673ece48ad91242526ebbaeab1";
 const responseType = "token";
-const redirectUri = "http://localhost:3000/callback";
+const redirectUri = "http://localhost:3000/";
 const state = "cengamat";
 const scope = "playlist-modify-public";
 let accessToken;
@@ -24,5 +26,29 @@ export const Spotify = {
       window.location = accessUrl;
     }
   },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artist,
+          album: track.album,
+          uro: track.uri,
+        }));
+      });
+  },
 };
- Spotify();
