b72a3f41671e6798e293d02f72102690676e0d0f
cengamat <azurnaci@cultwinesltd.com>
2022-10-18 16:00:21 +0100

    surge

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = 'e50e28673ece48ad91242526ebbaeab1'; // Insert client ID here.
-const redirectUri = 'http://localhost:3000/'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
+const redirectUri = 'http://cengospoty.surge.sh/'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken;
 
 const Spotify = {
